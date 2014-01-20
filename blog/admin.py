70a5fbf96a098a9afe0f6d8f4eb5266f1c06257a
hessonit <hessonit@gmail.com>
2014-01-20 19:48:23 +0100

    add admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_filter = ['published', 'created']
+    search_fields = ['title', 'description', 'content']
+    date_hierarchy = 'created'
+    save_on_top = True
+    prepopulated_fields = {"slug": ("title",)}
+
+
+
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
